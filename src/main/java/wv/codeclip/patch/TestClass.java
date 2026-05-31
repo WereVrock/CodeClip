f7a2b9f9a1a19c052c5feb87e9d7be7abaaa727f
bugrabeyaz <bugrabeyaz@yahoo.com>
2026-05-31 20:32:47 +0300

    increased tolerance working stntax

--- src/main/java/wv/codeclip/patch/TestClass.java
+++ src/main/java/wv/codeclip/patch/TestClass.java
@@ -1,35 +1,5 @@
-package wv.codeclip.patch;
-
-public class TestClass {
-
-    public static String hello() {
-        return "hello - base version for fuzzy tests";
-    }
-
-    public static String goodbye() {
-        return "goodbye - base version";
-    }
-
-    public static String fuzzyOne() {
-        // this method has trailing spaces in its comment    
-        return "fuzzyOne original";
-    }
-
-    public static String fuzzyTwo() {
-
-
-        return "fuzzyTwo original";
-    }
-
-    public static String fuzzyThree() {
-		return "fuzzyThree original";	// tabs used here
-    }
-
-    public static String fuzzyFour() {
-        return "fuzzyFour original";
-    }
-
-    public static String fuzzyFive() {
-        return "fuzzyFive original";
-    }
-}
+package wv.codeclip.patch; public class TestClass { public static String hello() { return "hello - base version for fuzzy tests"; } public static String goodbye() { return "goodbye - base version"; } public static String fuzzyOne() { // this method has trailing spaces in its comment return "fuzzyOne original"; } public static String fuzzyTwo() { return "fuzzyTwo UPDATED - blank lines collapsed"; }    public static String fuzzyThree() {
+        return "fuzzyThree UPDATED - tab normalisation works";
+    }     public static String fuzzyFour() {
+        return "fuzzyFour UPDATED - indent stripping works";
+    } public static String fuzzyFive() { return "fuzzyFive original"; } }
